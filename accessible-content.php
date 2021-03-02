0bb682a2af29b775b18aac493babf9dfb5c8ee3c
vladislav <ve@incsub.com>
2021-03-02 04:14:17 +0100

    Update version number

--- accessible-content.php
+++ accessible-content.php
@@ -8,7 +8,7 @@
 /**
  * Plugin Name: Accessible Content
  * Description: Check your pages and posts for potential accessibility issues and get help with ensuring your content is accessible.
- * Version: 1.0.1
+ * Version: 1.1.0
  * Text Domain: campus-a11y
  * Author: CampusPress
  * Author URI:  https://campuspress.com
@@ -16,7 +16,7 @@
 
 namespace Campus\A11y;
 
-define( 'CAMPUS_A11Y_VERSION', '1.0.1' );
+define( 'CAMPUS_A11Y_VERSION', '1.1.0' );
 
 define( __NAMESPACE__ . '\PLUGIN_FILE', __FILE__ );
 define( __NAMESPACE__ . '\PLUGIN_DIR', basename( dirname( __FILE__ ) ) );
